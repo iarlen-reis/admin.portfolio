dcdedf915c45e94f1e766932d3c0fa5fa8c99613
Iarlen <iarlensr@hotmail.com>
2024-03-24 17:30:45 -0300

    refactor: add react-router-dom as pages router.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LoginScreen from '@/screen/Login/index.tsx'
+import PrivateRoute from './components/PrivateRoute/index.tsx'
+import RootScreen from '@/screen/Root/index.tsx'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import React from 'react'
 import './globals.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootScreen />,
+    children: [
+      {
+        path: '/',
+        element: (
+          <PrivateRoute>
+            <App />
+          </PrivateRoute>
+        ),
+      },
+      { path: '/login', element: <LoginScreen /> },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
