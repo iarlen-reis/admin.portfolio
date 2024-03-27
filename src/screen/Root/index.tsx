28010d579e4c4d0693b9421f9e1f05cafe455e8e
Iarlen <iarlensr@hotmail.com>
2024-03-27 01:50:33 -0300

    feat: add QueryClientProvider on layout.

--- src/screen/Root/index.tsx
+++ src/screen/Root/index.tsx
@@ -1,14 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from '@/components/ui/sonner'
 import { Outlet } from 'react-router-dom'
 import Header from '@/components/Header'
 
 export default function RootScreen() {
+  const client = new QueryClient()
+
   return (
     <div className="w-full">
       <Header />
-      <main className="container mx-auto px-3">
-        <Outlet />
-      </main>
+      <QueryClientProvider client={client}>
+        <main className="container mx-auto px-3">
+          <Outlet />
+        </main>
+      </QueryClientProvider>
       <Toaster />
     </div>
   )
